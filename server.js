218a65d8dcf25c51529b4941ee7ed7f0ec052848
kuwood <p4nic510@gmail.com>
2016-08-31 12:40:52 -0600

    Add auth endpoints, db models, myclips endpoint, scraper and tests

--- server.js
+++ server.js
@@ -1,9 +1,177 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const authConfig = require('./oauth')
+const User = require('./models/user-model')
+const Clip = require('./models/clip-model')
+const passport = require("passport")
+const cookieParser = require("cookie-parser")
+const cookieSession = require("cookie-session")
+const request = require("request")
+const cheerio = require("cheerio")
+
+const dbConfig = require('./dbConfig')
 
 const app = express()
+const jsonParser = bodyParser.json()
+const Strategy = require("passport-twitch").Strategy
+
+app.use(cookieParser());
+app.use(cookieSession({
+    secret: "somesecrettokenhere"
+}));
+
+// parse application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.use(passport.initialize())
+app.use(passport.session());
 
 app.use(express.static('public'))
 
-app.listen(process.env.PORT || 8080)
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use(new Strategy({
+        name: 'twitch',
+        clientID: process.env.clientID || authConfig.twitch.clientID,
+        clientSecret: process.env.clientSecret || authConfig.twitch.clientSecret,
+        callbackURL: process.env.callbackURL || authConfig.twitch.callbackURL,
+        scope: "user_read"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOne({
+            twitchId: profile.id
+        }, function(err, user) {
+            console.log(err, user);
+            if (user) {
+                return done(err, user);
+            } else {
+                user = new User({
+                    twitchId: profile.id,
+                    username: profile.username,
+                    clips: []
+                })
+                user.save(function(err, user) {
+                    console.log(err, user);
+                    done(err, user)
+                })
+            }
+        });
+
+    }
+));
+
+app.get("/clips", function(req, res) {
+    Clip.find({}),
+    function(err, clip) {
+        console.log(req.clip);
+        res.status(200).json(req.clip)
+    };
+})
+
+app.get("/auth/twitch", passport.authenticate('twitch'),
+    function(req, res) {
+        // Request will be redirected to Twitch.tv for authentication, so this
+        // function will not be called.
+    }
+);
+
+
+app.get("/auth/twitch/callback", passport.authenticate("twitch", {
+    failureRedirect: "/"
+}), function(req, res) {
+    // Successful authentication, redirect home.
+    //req.session.user = req.user
+    //console.log(req.query);
+    //console.log(req.body);
+    res.sendStatus(200);
+});
+
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/')
+});
+
+app.get("/account", ensureAuthenticated, function(req, res) {
+    res.sendStatus(200)
+})
+
+app.get("/myclips", ensureAuthenticated, function(req, res) {
+    User.find({
+        twitchId: req.user.twitchId
+    }, function(err, user) {
+        res.status(200).json(req.user.clips)
+    });
+})
+//need to add new clip to frontend myclips list
+app.post("/scrape", function(req, res) {
+    url = req.body.link
+    console.log(req.body);
+    request(url, function(error, response, html) {
+        if (!error) {
+            let $ = cheerio.load(html)
+
+            let userClip = new Clip();
+            userClip.title = req.body.title
+            userClip.img = $('.clip').attr('poster')
+            userClip.author = $('.curator-link').text()
+            userClip.game = $('.broadcaster-info__game-link').text()
+            userClip.date = new Date()
+            userClip.link = req.body.link
+            userClip.twitchId = req.user.twitchId
+            userClip.save()
+
+            res.status(201).json(userClip)
+        } else {
+            res.status(500).json(error)
+        }
+    })
+})
+
+/*app.post("/clips", ensureAuthenticated, function(req, res) {
+    Clip.create({
+
+    })
+})*/
+
+// test authentication middleware
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.sendStatus(401);
+}
+
+const runServer = callback => {
+    mongoose.connect(dbConfig.DATABASE_URL, err => {
+        if (err && callback) {
+            return callback(err)
+        }
+
+        app.listen(dbConfig.PORT, () => {
+            console.log('Listening on localhost:' + dbConfig.PORT);
+            if (callback) {
+                callback()
+            }
+        })
+    })
+}
+
+if (require.main === module) {
+    runServer(err => {
+        if (err) {
+            console.error(err)
+        }
+    })
+}
 
 exports.app = app
+exports.runServer = runServer
--- test/test-server.js
+++ test/test-server.js
@@ -1,7 +1,12 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+const passport = require("passport")
+const User = require('../models/user-model')
+const Clip = require('../models/clip-model')
 
+const mongoose = require('mongoose')
 const server = require('../server.js')
+
 
 const should = chai.should()
 const app = server.app
@@ -9,12 +14,119 @@ const app = server.app
 chai.use(chaiHttp)
 
 describe('Index page', () => {
-    it('exists', (done) => {
+    it('exists', done => {
         chai.request(app)
             .get('/')
-            .end((err,res) => {
+            .end((err, res) => {
                 res.should.have.status(200)
                 res.should.be.html
+                done()
+            })
+    })
+})
+
+describe('Authentication', () => {
+
+    before(done => {
+        mongoose.connect('mongodb://localhost/top-clips-dev');
+
+        // Allows the middleware to think we're already authenticated.
+        app.request.isAuthenticated = () => {
+            return true
+        }
+        done()
+    })
+
+    after(done => {
+        app.request.isAuthenticated = () => {
+            return false
+        }
+        mongoose.connection.close()
+        done()
+    })
+    beforeEach(done => {
+        app.request.user = {
+            twitchId: 1,
+            username: "one",
+            clips: []
+        }
+        done()
+    });
+
+    afterEach(done => {
+        done()
+    });
+
+    it('should allow access to account endpoint when logged in', done => {
+        chai.request(app)
+            .get('/account')
+            .end((err, res) => {
+                res.should.have.status(200)
+                done()
+            })
+    })
+    it('should allow access to myclips endpoint', done => {
+        chai.request(app)
+            .get('/myclips')
+            .end((err, res) => {
+                res.should.have.status(200)
+                res.body.should.be.a('array')
+                done()
+            })
+    })
+    it('should logout and redirect to homepage', done => {
+        chai.request(app)
+            .get('/logout')
+            .end((err, res) => {
+                res.should.be.html
+                done()
+            })
+    })
+})
+
+describe('Not authenticated', () => {
+    it('should not allow access to account endpoint', done => {
+        chai.request(app)
+            .get('/account')
+            .end((err, res) => {
+                res.should.have.status(401)
+                done()
+            })
+    })
+})
+
+describe('Clips', () => {
+    before(done => {
+        mongoose.connect('mongodb://localhost/top-clips-dev');
+        done()
+    })
+
+    after(done => {
+        mongoose.connection.close()
+        done()
+    })
+    beforeEach(done => {
+        Clip.remove({}, err => {
+            done()
+        })
+    })
+
+    it('should scrape data from url and create a clip object', done => {
+        let clip = {title: 'test', link: 'https://clips.twitch.tv/dota2ti/SmilingCaribouTwitchRaid'}
+        chai.request(app)
+            .post('/scrape')
+            .send(clip)
+            .end((err, res) => {
+                res.should.have.status(201)
+                res.body.should.be.a('object')
+                res.body.should.have.property('title')
+                res.body.should.have.property('img')
+                res.body.should.have.property('author')
+                res.body.should.have.property('game')
+                res.body.should.have.property('date')
+                res.body.should.have.property('link')
+                res.body.should.have.property('twitchId')
+                res.body.should.have.property('_id')
                 done()
             })
     })
